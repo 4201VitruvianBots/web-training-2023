cac5899a5584332e3198cc9790a8cfbf9c8947eb
Sheraz Fayyaz <Sheraz24132@gmail.com>
2023-09-24 18:32:04 -0700

    Initial training code

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,30 @@
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+    const [clicked, setClicked] = useState(false);
     return (
         <>
             <h1>[your app name here]</h1>
             <p>Time to make your very own quiz.</p>
+            <p>owrihwriwrwerwerwe</p>
+            <p>owieruweoiruweoiruewiruweirwoierwuro</p>
+            <p>heheheha</p>     
+            <button onClick={() => setClicked(true)}>
+                ..yrdxrewyutuiouryugfdghjhbvnm,
+            </button>
+            <button> nuh uh </button>
+            <div>
+                <h2>nuh uhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh</h2>
+                <p>he he he yup</p>
+            </div>
+            <p>
+                qqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqq
+                qqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqq
+                qqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqq
+            </p>
+
+            
         </>
     );
 }

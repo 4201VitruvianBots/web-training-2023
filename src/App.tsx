42a654b3831f5d981886c00a7c6b774476de01f5
Kyle Tsubaki <kylekaos03@gmail.com>
2023-09-24 18:31:12 -0700

    Sept 24 learning first day

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,26 @@
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+    const[clicked, setClicked] = useState(false);
     return (
         <>
-            <h1>[your app name here]</h1>
-            <p>Time to make your very own quiz.</p>
+            <h1>[Cheese & Quackers Cooking Website]</h1>
+            <p>This is NOT a cooking mama ripoff</p>
+            <p>fdasfdasfkasl;fjasfiajeirqjkasfhu67869768968</p>
+            <p>enter your credit card number here:</p>
+            <button onClick={() => setClicked(true)}> big grey button</button>
+``
+
+            <div>
+                <h2>Section 1</h2>
+                <p> hallo</p>
+                <p>i am
+                  <br />a minion
+                </p>
+            </div>
         </>
     );
 }
 
-export default App;
+export default App;                                                             
